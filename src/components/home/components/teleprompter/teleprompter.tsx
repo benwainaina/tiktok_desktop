39835256838849f1d5bf334ed5182c43439804a6
Ben Wainaina <bwainaina@curalinc.com>
2026-04-08 19:47:28 +0300

    add youtube banner

--- src/components/home/components/teleprompter/teleprompter.tsx
+++ src/components/home/components/teleprompter/teleprompter.tsx
@@ -2,29 +2,47 @@ import { useState } from "react";
 import Marquee from "react-fast-marquee";
 import "./teleprompter.component.css";
 import { EXTERNAL_CONSTANTS } from "../../../constants/constants";
+import { ImageComponent } from "../../shared/components/image.component";
 
 export const TeleprompterComponent = ({ gameName }: any) => {
   const [advertContent, setAdvertContent] = useState<any[]>([
     { title: `Game name = ${gameName}.` },
     { title: EXTERNAL_CONSTANTS.message },
-    { title: "No inappropriate / unethical comments!" },
     { title: "love one another." },
     { title: "remember your creator in the days of your youth." },
   ]);
 
   return (
     <div className="advertContent__topWrapper">
-      <Marquee speed={50} direction="right">
+      <Marquee speed={33} direction="right">
         <div className="advertContent">
-          {advertContent.map((content, index) => (
-            <AdvertContentComponent
-              key={index}
-              payload={{
-                ...content,
-                isLast: index === advertContent.length - 1,
-              }}
-            />
-          ))}
+          {advertContent.map((content, index) => {
+            if (index === 0 || index === 2) {
+              return (
+                <>
+                  <div className="advertContent__wrapper advertContent__wrapper__youtubeBanner">
+                    <ImageComponent uri="https://iili.io/BcxpeAN.png" />
+                  </div>
+                  <AdvertContentComponent
+                    key={index}
+                    payload={{
+                      ...content,
+                      isLast: index === advertContent.length - 1,
+                    }}
+                  />
+                </>
+              );
+            }
+            return (
+              <AdvertContentComponent
+                key={index}
+                payload={{
+                  ...content,
+                  isLast: index === advertContent.length - 1,
+                }}
+              />
+            );
+          })}
         </div>
       </Marquee>
     </div>
